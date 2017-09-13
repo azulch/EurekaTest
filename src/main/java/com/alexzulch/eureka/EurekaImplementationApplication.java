81e03d504b2e80bb92aa84d5f939d77209f0d0ae
azulch <alexander.zulch@cgius.com>
2017-09-13 14:04:22 -0600

    initial

---
+++ src/main/java/com/alexzulch/eureka/EurekaImplementationApplication.java
@@ -0,0 +1,14 @@
+package com.alexzulch.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaImplementationApplication.class, args);
+	}
+}
